9c909e290d8523dc182a37d8b889d9aee7e80d34
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2024-10-03 12:11:02 +0100

    image fix 7

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Hero from "./components/Hero";
 import Card from "./components/Card";
 import data from "./data";
 
-import "/src/assets/airbnb-logo.png";
+/* import "/src/assets/airbnb-logo.png";
 import "/src/assets/photo-grid.png";
 import "/src/assets/katie-zaferes.png";
 import "/src/assets/star.png";
 import "/src/assets/mountain-bike.png";
-import "/src/assets/wedding-photography.png";
+import "/src/assets/wedding-photography.png"; */
 
 import "./styles/style.css";
 import Footer from "./components/Footer";
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,9 +11,9 @@ export default function Card(props) {
 	return (
 		<div className="card">
 			{badgeText && <div className="card--badge">{badgeText}</div>}
-			<img src={`/src/assets/${props.coverImg}`} className="card--image" />
+			<img src={`/public/images/${props.coverImg}`} className="card--image" />
 			<div className="card--stats">
-				<img src="/src/assets/star.png" className="card--star" />
+				<img src="/public/images/star.png" className="card--star" />
 				<span>{props.stats.rating}</span>
 				<span className="gray">({props.stats.reviewCount}) • </span>
 				<span className="gray">{props.location}</span>
