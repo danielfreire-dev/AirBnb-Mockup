3bfc7c4b1a8ea54404a59ee7d11cfe9a46c43ae3
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2024-10-03 12:04:01 +0100

    image fix 6

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import Hero from "./components/Hero";
 import Card from "./components/Card";
 import data from "./data";
 
+import "/src/assets/airbnb-logo.png";
+import "/src/assets/photo-grid.png";
+import "/src/assets/katie-zaferes.png";
+import "/src/assets/star.png";
+import "/src/assets/mountain-bike.png";
+import "/src/assets/wedding-photography.png";
+
 import "./styles/style.css";
 import Footer from "./components/Footer";
 
